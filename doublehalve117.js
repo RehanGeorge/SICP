77507d8959f1ac9e250d19414cb7ed92c9851b53
Rehan George <rehan.george@gmail.com>
2024-02-05 23:07:51 +0530

    Add 1.17

---
+++ doublehalve117.js
@@ -0,0 +1,16 @@
+function times(a, b) {
+  return b === 0 ? 0 : a + times(a, b - 1);
+}
+
+function double(x) {
+  return 2 * x;
+}
+
+function halve(x) {
+  return x / 2;
+}
+
+function multiplier(a, b) {
+  return b % 2 === 0 ? double(times(a, halve(b))) : a + times(a, halve(b - 1));
+}
+\
